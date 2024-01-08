34ce3f0e98a7a5619db15f6625b3e0c522961055
HathilThahee <hathilthahee430@gmail.com>
2024-01-08 14:02:12 +0530

    disabled contextmenu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,11 @@
 $("document").ready(function () {
+  // Disable right click
+  $('body').bind('contextmenu', function (e) {
+    return false;
+  });
+
+
+  // Menu Bar toggle
   $("menuBtn > svg").text(
     `<path fill="currentColor" d="M8.4 17L7 15.6l3.6-3.6L7 8.425l1.4-1.4l3.6 3.6l3.575-3.6l1.4 1.4l-3.6 3.575l3.6 3.6l-1.4 1.4L12 13.4L8.4 17Z"/>`
   );
