d7fa016dbb0768ed8b1a49033493c8c09f071b5c
HathilThahee <hathilthahee430@gmail.com>
2024-01-20 13:05:45 +0530

    feat: adding animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,7 @@ $("document").ready(function () {
   });
 
   // Toggle Trigger button
+  $('#triggerBtn').hide();
   function triggerBtn(){
     if(window.scrollY < 100){
       $('#triggerBtn').hide();
